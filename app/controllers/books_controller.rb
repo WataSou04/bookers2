db8a77befb838fc3470f68b8bb137ed8281da6c2
EC2 Default User <ec2-user@ip-172-31-37-30.ap-northeast-1.compute.internal>
2024-01-18 06:00:00 +0000

    [Fit]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,7 +11,7 @@ class BooksController < ApplicationController
     @book.user_id = current_user.id
     if @book.save
       flash[:notice] = "You have created book successfully"
-      redirect_to books_path
+      redirect_to book_path(book.id)
     else
       render :index
     end
@@ -25,6 +25,7 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+    @new_book = Book.new
     @user = current_user
   end
   
